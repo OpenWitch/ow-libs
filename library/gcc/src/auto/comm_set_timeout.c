43aa5c0670203d275a2269a53486f4c2395261fd
Adrian Siekierka <kontakt@asie.pl>
2024-12-14 17:22:04 +0100

    headers: fix comm_set_timeout

--- library/gcc/src/auto/comm_set_timeout.c
+++ library/gcc/src/auto/comm_set_timeout.c
@@ -5,7 +5,7 @@ void comm_set_timeout(uint16_t recv_timeout, uint16_t send_timeout) {
 	__asm volatile (
 		"int $0x14"
 		: "=a" (result)
-		: "b" (send_timeout), "Rah" ((uint8_t) 0x08)
+		: "b" (recv_timeout), "c" (send_timeout), "Rah" ((uint8_t) 0x08)
 		: "cc", "memory"
 	);
 }
