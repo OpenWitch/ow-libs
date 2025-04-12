959fddb456149b943f217ed3c3f68478d1bef388
Adrian Siekierka <kontakt@asie.pl>
2025-04-12 21:05:33 +0200

    filesys: add comments

--- library/gcc/include/sys/filesys.h
+++ library/gcc/include/sys/filesys.h
@@ -5,17 +5,53 @@
 
 /* Begin auto-generated section */
 
+/* 
+ * Maximum file name length.
+ * 
+*/
 #define MAXFNAME 16
+/* 
+ * Maximum file path length.
+ * 
+*/
 #define MAXPATHLEN 64
+/* 
+ * Maximum file description length.
+ * 
+*/
 #define MAXFINFO 24
 
+/* 
+ * File mode flag: Execute
+ * 
+*/
 #define FMODE_X 0x01
+/* 
+ * File mode flag: Write
+ * 
+*/
 #define FMODE_W 0x02
+/* 
+ * File mode flag: Read
+ * 
+*/
 #define FMODE_R 0x04
 #define FMODE_MMAP 0x08
 #define FMODE_STREAM 0x10
+/* 
+ * File mode flag: Indirect Library
+ * 
+*/
 #define FMODE_ILIB 0x20
+/* 
+ * File mode flag: Link
+ * 
+*/
 #define FMODE_LINK 0x40
+/* 
+ * File mode flag: Directory
+ * 
+*/
 #define FMODE_DIR 0x80
 
 #define E_FS_SUCCESS 0
