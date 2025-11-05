776e2650776f55a061f7b95e3670e0b6edf443c7
Adrian Siekierka <kontakt@asie.pl>
2025-11-05 16:55:00 +0100

    timer: fix timer_get_count

--- library/gcc/src/auto/timer_get_count.c
+++ library/gcc/src/auto/timer_get_count.c
@@ -1,6 +1,6 @@
 #include <sys/types.h>
 
-void timer_get_count(uint8_t type) {
+uint16_t timer_get_count(uint8_t type) {
 	uint16_t result;
 	__asm volatile (
 		"int $0x16"
@@ -8,4 +8,5 @@ void timer_get_count(uint8_t type) {
 		: "a" ((uint16_t) (((0x09) << 8) | (type & 0xFF)))
 		: "cc", "memory"
 	);
+	return (uint16_t) result;
 }
