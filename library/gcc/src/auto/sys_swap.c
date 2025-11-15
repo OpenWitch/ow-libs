b354b3c4d33b1049eaa902cccbecf1f258fc2f6a
Adrian Siekierka <kontakt@asie.pl>
2025-11-15 08:54:13 +0100

    headers: add sys_suspend/resume/swap

---
+++ library/gcc/src/auto/sys_swap.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+
+uint16_t sys_swap(uint8_t slot) {
+	uint16_t result;
+	__asm volatile (
+		"int $0x17"
+		: "=a" (result)
+		: "a" ((uint16_t) (((0x13) << 8) | (slot & 0xFF)))
+		: "cc", "memory"
+	);
+	return (uint16_t) result;
+}
